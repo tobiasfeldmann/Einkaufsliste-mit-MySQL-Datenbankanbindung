21527f6f5a82f6dda933580bd30ecc5a87bb39bd
tobiasfeldmann <132939959+tobiasfeldmann@users.noreply.github.com>
2023-08-21 19:55:55 +0200

    Anpassungen GUI.

--- src/main/Web/script.js
+++ src/main/Web/script.js
@@ -1,3 +1,29 @@
-const fr = new FileReader();
 
-fr.readAsText(fileInput.file)
+const inputBox = document.getElementById("input-box");
+
+inputBox.addEventListener("keypress", function(event) {
+    if(event.key === "Enter") {
+        addTask();
+    }
+});
+
+function addTask() {
+    if(inputBox.value === "") {
+        alert("Keine Leeren Eingaben bitte dankeee");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+    }
+    inputBox.value = "";
+}
+
+const listContainer = document.getElementById("list-container");
+listContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    }
+}, false);
+
+
