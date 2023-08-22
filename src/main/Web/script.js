8b7b605e04e0f6e1a9df756f6cc656f8ac841d99
tobiasfeldmann <132939959+tobiasfeldmann@users.noreply.github.com>
2023-08-22 21:43:45 +0200

    Responsives Design für Smartphones implementiert. Ergänzung eines Kreuzes um Eintrag aus der Liste zu löschen.

--- src/main/Web/script.js
+++ src/main/Web/script.js
@@ -15,15 +15,33 @@ function addTask() {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
     inputBox.value = "";
+    saveData();
 }
 
 const listContainer = document.getElementById("list-container");
 listContainer.addEventListener("click", function(e) {
     if(e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
     }
 }, false);
 
 
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showListFromLocalStorage() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showListFromLocalStorage();
